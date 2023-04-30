cb3377499de661fda0719c6c7d17fd2e6f992a0a
Enitsa <k.mazur9@wp.pl>
2023-04-30 21:44:13 +0200

    Revert "changes"
    This reverts commit c648b476bfe3781a7f6f1f2481ba2b558d9a3fcc.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from routes import init_routes
+from helpers import *
 
 app = Flask(__name__)
-init_routes(app)  
-    
-if __name__ == '__main__':
-    print(app.url_map)
+init_routes(app)
+
+
+
+if __name__ == "__main__":
+    # print(app.url_map)
     app.run(debug=True)
--- routes.py
+++ routes.py
@@ -1,37 +1,45 @@
-from flask import render_template, request 
+from flask import render_template, request
 import requests
-from helpers import forward_response
+from helpers import forward_response, get_annocements, filter_annoucements
+
 
 def init_routes(app):
-    @app.route('/test')
-    def test(): 
-        return render_template('test.html')
-    
-    @app.route('/')
+    @app.route("/test")
+    def test():
+        return render_template("test.html")
+
+    @app.route("/", methods=["GET", "POST"])
     def main():
-        announcements = requests.get('https://chatty-bulldog-76.telebit.io/announcements').json()
+        filter = ""
+        if 'filter' in request.form.keys():
+            filter = request.form['filter']
+        announcements = filter_annoucements(filter)
+        
         for a in announcements:
-            a['short_title']=a['title']
-            if len(a['title']) > 40:
-                a['short_title']=a['title'][:40]+'...'   
-        return render_template('index.html', announcements=announcements)
+            a["short_title"] = a["title"]
+            if len(a["title"]) > 25:
+                a["short_title"] = a["title"][:25] + "..."
+                
+        print("Zwrócone rekordy spełniające kryterium "+filter+" "+str(len(announcements)))
+                
+        return render_template("index.html", announcements=announcements)
 
-    @app.route('/announcements', methods=["GET"])
+    @app.route("/announcements", methods=["GET"])
     def announcements():
-        r = requests.get('https://chatty-bulldog-76.telebit.io/announcements')
-        return r.json()
+        return get_annocements()
 
-    @app.route('/login', methods=["POST"])
+    @app.route("/login", methods=["POST"])
     def login():
         r = requests.post(
-            'https://chatty-bulldog-76.telebit.io/login',
-            headers={'Content-Type': 'application/json'},
-            json={'user': 't2',
-                'password': 't1'}                  
+            "https://chatty-bulldog-76.telebit.io/login",
+            headers={"Content-Type": "application/json"},
+            json={"user": "t2", "password": "t1"},
         )
         return forward_response(r)
 
-    @app.route('/my_account', methods=["GET"])
+    @app.route("/my_account", methods=["GET"])
     def my_account():
-        r = requests.get('https://chatty-bulldog-76.telebit.io/my_account', cookies=request.cookies)
+        r = requests.get(
+            "https://chatty-bulldog-76.telebit.io/my_account", cookies=request.cookies
+        )
         return r.json()
