c648b476bfe3781a7f6f1f2481ba2b558d9a3fcc
Enitsa <k.mazur9@wp.pl>
2023-04-30 21:33:49 +0200

    changes
    Co-authored-by: kubastudent121 <kubastudent121@users.noreply.github.com>
    Co-authored-by: MatiHill <MatiHill@users.noreply.github.com>

--- helpers.py
+++ helpers.py
@@ -1,23 +1,9 @@
 from flask import make_response, jsonify
-import requests
 
 
 def forward_response(r):
     cookies = r.cookies.get_dict()
-    resp = make_response(jsonify(r.json()), r.status_code)
+    resp = make_response(jsonify(r.json()),r.status_code)
     for c in cookies:
         resp.set_cookie(c, cookies[c])
     return resp
-
-
-def get_annocements():
-    return requests.get("https://chatty-bulldog-76.telebit.io/announcements").json()
-
-
-def filter_annoucements(search_for: str):
-    annocements = get_annocements()
-    filtered = []
-    for a in annocements:
-        if str(a.values()).find(search_for) >= 0:
-            filtered.append(a)
-    return filtered
--- routes.py
+++ routes.py
@@ -1,45 +1,37 @@
-from flask import render_template, request
+from flask import render_template, request 
 import requests
-from helpers import forward_response, get_annocements, filter_annoucements
-
+from helpers import forward_response
 
 def init_routes(app):
-    @app.route("/test")
-    def test():
-        return render_template("test.html")
-
-    @app.route("/", methods=["GET", "POST"])
+    @app.route('/test')
+    def test(): 
+        return render_template('test.html')
+    
+    @app.route('/')
     def main():
-        filter = ""
-        if 'filter' in request.form.keys():
-            filter = request.form['filter']
-        announcements = filter_annoucements(filter)
-        
+        announcements = requests.get('https://chatty-bulldog-76.telebit.io/announcements').json()
         for a in announcements:
-            a["short_title"] = a["title"]
-            if len(a["title"]) > 25:
-                a["short_title"] = a["title"][:25] + "..."
-                
-        print("Zwrócone rekordy spełniające kryterium "+filter+" "+str(len(announcements)))
-                
-        return render_template("index.html", announcements=announcements)
+            a['short_title']=a['title']
+            if len(a['title']) > 40:
+                a['short_title']=a['title'][:40]+'...'   
+        return render_template('index.html', announcements=announcements)
 
-    @app.route("/announcements", methods=["GET"])
+    @app.route('/announcements', methods=["GET"])
     def announcements():
-        return get_annocements()
+        r = requests.get('https://chatty-bulldog-76.telebit.io/announcements')
+        return r.json()
 
-    @app.route("/login", methods=["POST"])
+    @app.route('/login', methods=["POST"])
     def login():
         r = requests.post(
-            "https://chatty-bulldog-76.telebit.io/login",
-            headers={"Content-Type": "application/json"},
-            json={"user": "t2", "password": "t1"},
+            'https://chatty-bulldog-76.telebit.io/login',
+            headers={'Content-Type': 'application/json'},
+            json={'user': 't2',
+                'password': 't1'}                  
         )
         return forward_response(r)
 
-    @app.route("/my_account", methods=["GET"])
+    @app.route('/my_account', methods=["GET"])
     def my_account():
-        r = requests.get(
-            "https://chatty-bulldog-76.telebit.io/my_account", cookies=request.cookies
-        )
+        r = requests.get('https://chatty-bulldog-76.telebit.io/my_account', cookies=request.cookies)
         return r.json()
