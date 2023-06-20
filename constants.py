11d77f20f7dcd40cb986de6b77d90091c0ac3e55
Etnisa <Etnisa@users.noreply.github.com>
2023-06-20 19:31:35 +0200

    bugfix modal style
    Signed-off-by: Etnisa <Etnisa@users.noreply.github.com>

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 MAX_TITLE_LEN = 70
-CARDS_PER_PAGE = 10  # dla testów potem się ustawi na ~15
+CARDS_PER_PAGE = 123  # dla testów potem się ustawi na ~15
 AVATAR_CACHE_SIZE = 64
-API = "https://chatty-bulldog-76.telebit.io"
+API = "https://chatty-bulldog-76.telebit.io/api"
